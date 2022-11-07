cec4d22e7195a26519b0e47211c2bd246cf507f7
Kurt Thiemann <kurt@aternos.org>
2022-11-07 16:58:22 +0100

    fix reading/writing BigInts in browsers that don't support BigInt at all

--- src/Util/BigInt.js
+++ src/Util/BigInt.js
@@ -1,6 +1,9 @@
+
+export const nativeBigIntSupport = typeof BigInt === 'function';
+
 /*
 Simply replacing BigInt works in the context of this library,
 as all the values read as BigInt can only be used as regular Numbers anyway,
 since both DataView and FileReader do not support BigInt offset and length parameters.
  */
-export default (typeof BigInt === 'function' ? BigInt : Number);
+export default (nativeBigIntSupport ? BigInt : Number);
--- src/Util/BigIntUtils.js
+++ src/Util/BigIntUtils.js
@@ -1,4 +1,5 @@
-import BigInt from "./BigInt.js";
+import BigInt, {nativeBigIntSupport} from './BigInt.js';
+import Constants from '../Constants.js';
 
 export default class BigIntUtils {
     /**
@@ -9,13 +10,17 @@ export default class BigIntUtils {
      * @return {number|BigInt}
      */
     static getBigUint64FromView(dataView, byteOffset, littleEndian, native = true) {
-        if(DataView.prototype.getBigUint64 && native) {
+        if (DataView.prototype.getBigUint64 && native) {
             return dataView.getBigUint64(byteOffset, littleEndian);
         }
         const [h, l] = littleEndian ? [4, 0] : [0, 4];
         const wh = BigInt(dataView.getUint32(byteOffset + h, littleEndian));
         const wl = BigInt(dataView.getUint32(byteOffset + l, littleEndian));
-        return (wh << BigInt(32)) + wl;
+
+        if (nativeBigIntSupport) {
+            return (wh << BigInt(32)) + wl;
+        }
+        return wh * BigInt(Constants.MAX_UINT32) + wl;
     }
 
     /**
@@ -27,11 +32,18 @@ export default class BigIntUtils {
      * @returns {void}
      */
     static setBigUint64InView(dataView, byteOffset, value, littleEndian, native = true) {
-        if(DataView.prototype.setBigUint64 && native) {
+        if (DataView.prototype.setBigUint64 && native) {
             return dataView.setBigUint64(byteOffset, value, littleEndian);
         }
-        const wh = Number((value >> BigInt(32)) & BigInt(0xFFFFFFFF));
-        const wl = Number(value & BigInt(0xFFFFFFFF));
+
+        let wh, wl;
+        if (nativeBigIntSupport) {
+            wh = Number((value >> BigInt(32)) & BigInt(0xFFFFFFFF));
+            wl = Number(value & BigInt(0xFFFFFFFF));
+        } else {
+            wh = Math.floor(Number(value) / Constants.MAX_UINT32);
+            wl = Number(value) - wh * Constants.MAX_UINT32;
+        }
         const [h, l] = littleEndian ? [4, 0] : [0, 4];
         dataView.setUint32(byteOffset + h, wh, littleEndian);
         dataView.setUint32(byteOffset + l, wl, littleEndian);
